a4005a82986f5c363fd97d6fa241fb40a7e61708
Michael Owolabi <michael.i.owolabi@gmail.com>
2019-11-19 17:26:38 +0100

    implement app navigation bar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Standup from '../components/Standup/Standup';
 import './App.css';
+import NavBar from '../components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Standup />
+        <NavBar name="Standup" />
+        {/* <Standup /> */}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './index.css';
 import App from './containers/App';
 import Setup from './components/Setup/Setup';
 import Standup from './components/Standup/Standup';
